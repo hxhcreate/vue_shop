b6921c3705ba93da75082b00c69f737a02b305ad
hxhcreate <hxh_create@163.com>
2022-03-13 00:36:10 +0800

    Login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
+import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(router).mount('#app')
+import * as icons from "@element-plus/icons-vue"
+
+import './assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+
+
+const app = createApp(App)
+app.config.globalProperties.$http = axios
+Object.keys(icons).forEach(key =>{
+    app.component(key, icons[key])
+})
+installElementPlus(app)
+app.use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from "../components/Login.vue"
+import Home from "../components/Home.vue"
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    redirect: "/login"
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/login",
+    name:"Login",
+    component: Login
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
